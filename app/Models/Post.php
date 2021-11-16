2eb2d4ab5ddbc84362a29d380d7b73a0f0c93a5d
MorenaFischione <fischione.morena@gmail.com>
2021-11-16 17:55:13 +0100

    modified AppServiceProvider

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ class Post extends Model
 {
     //
     protected $fillable = ['title', 'author', 'post_content', "post_date"];
+
+
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
 }
