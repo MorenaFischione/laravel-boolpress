3a5a9fefbb3c6993566d341868d91e0667c28425
MorenaFischione <fischione.morena@gmail.com>
2021-11-16 18:59:32 +0100

    add old in input value to create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -20,11 +20,11 @@
                 @csrf
                 <div class="mb-3 form-group">
                     <label for="title" class="form-label">Titolo del post</label>
-                    <input type="text" id="title" class="form-control" placeholder="Inserisci il titolo del post" name="title" value={{$post->title}}>
+                    <input type="text" id="title" class="form-control" placeholder="Inserisci il titolo del post" name="title" value={{old('title', $post->title)}}>
                 </div>
 
                 <div class="mb-3 form-group">
-                    <label for="category_id" class="form-label">Category</label>
+                    <label for="category_id">Categoria</label>
                     <select name="category_id" id="category_id">
                         <option>Senza Categoria</option>
                         @foreach ($categories as $category)
@@ -36,7 +36,7 @@
 
                 <div class="mb-3 form-group">
                     <label for="author" class="form-label">Autore del post</label>
-                    <input type="text" id="author" class="form-control" placeholder="Inserisci l'autore del post" name="author" value={{$post->author}}>
+                    <input type="text" id="author" class="form-control" placeholder="Inserisci l'autore del post" name="author" value={{old('author', $post->author)}}>
                 </div>
                 <div class="mb-3 form-group">
                     <label for="post_date" class="form-label">Autore del post</label>
@@ -44,7 +44,7 @@
                 </div>
                 <div class="mb-3 form-group">
                     <label for="post_content" class="form-label">Inserisci il testo del post</label>
-                    <textarea type="textarea" id="post_content" class="form-control" placeholder="Inserisci il testo del post" name="post_content" >{{$post->post_content}}</textarea>
+                    <textarea type="textarea" id="post_content" class="form-control" placeholder="Inserisci il testo del post" name="post_content" >{{old('post_content',$post->post_content)}}</textarea>
                 </div>
 
 
